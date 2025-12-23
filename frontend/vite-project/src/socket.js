3f31b1ec712e278b823c056334b1bd9ba77f0722
BhavadharaniD <bhavadharani2027@gmail.com>
2025-12-23 14:20:07 +0530

    Update socket.js

--- frontend/vite-project/src/socket.js
+++ frontend/vite-project/src/socket.js
@@ -1,9 +1,12 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const SOCKET_URL = import.meta.env.VITE_API_URL;
+
+const socket = io(SOCKET_URL, {
   auth: {
-    token: localStorage.getItem("token")
-  }
+    token: localStorage.getItem("token"),
+  },
+  withCredentials: true,
 });
 
 export default socket;
